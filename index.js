c4f844a0d39f984bf9dfc717d9bed2b5cf1350f6
Nuwan Fernando <nuwaninfo@gmail.com>
2024-01-09 23:35:37 +0200

    3.2: Phonebook backend step2

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+
+const persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/api/info", function (request, response) {
+  const numOfPersons = `<p>Phonebook has info for ${persons.length} people</p>`;
+  const currentDateTime = `<p>${new Date().toString()}</p>`;
+  response.send(numOfPersons + currentDateTime);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
