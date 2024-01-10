4240f25f0e09393144af4ed641d65fa827747d20
Nuwan Fernando <nuwaninfo@gmail.com>
2024-01-10 17:36:31 +0200

    3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -38,13 +38,20 @@ app.get("/api/persons", (request, response) => {
 app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((person) => person.id === id);
-  console.log("test", typeof person);
 
   if (person) {
     response.json(person);
   } else {
     response.status(404).send("person not found").end();
   }
+});
+
+// Delete a person
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
